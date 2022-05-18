05aa6d127511fc35b25b4d79c140bbb12570dbfa
ChaseRensberger <chasehrensberger@gmail.com>
2022-05-18 11:18:19 -0400

    wow, i have a lot to do

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div className='navigation'>Navigation</div>
+    <div className='navigation'>
+      <div>About.</div>
+      <div>Design.</div>
+      <div>Film.</div>
+      <div>Audio.</div>
+      <div>Contact.</div>
+    </div>
   )
 }
 
