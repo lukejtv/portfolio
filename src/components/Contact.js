9fa2c157b2b00f59c3f2068ea30e1e4b55eb391d
ChaseRensberger <chasehrensberger@gmail.com>
2022-06-02 00:33:42 -0400

    add back contact for luke :) <3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,31 +3,27 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact">
-
+      <div className="contact-container">
+        <div className="contact-title-text">Contact</div>
+        <div className="contact-sub-text">
+          For freelance projects, portfolio questions, and more.
+        </div>
+        <input className="contact-name-and-email" placeholder="Name" />
+        <input className="contact-name-and-email" placeholder="Email" />
+        <textarea className="contact-message" placeholder="Message" />
+        <button
+          className="contact-submit"
+          onClick={() => {
+            alert(
+              "Sorry, contact is in development. For now please email me directly at luke.ab.johnson@gmail.com"
+            );
+          }}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Contact;
-
-
-
-      //  <div className="contact-container">
-      //   <div className="contact-title-text">Contact</div>
-      //   <div className="contact-sub-text">
-      //     For freelance projects, portfolio questions, and more.
-      //   </div>
-      //   <input className="contact-name-and-email" placeholder="Name" />
-      //   <input className="contact-name-and-email" placeholder="Email" />
-      //   <textarea className="contact-message" placeholder="Message" />
-      //   <button
-      //     className="contact-submit"
-      //     onClick={() => {
-      //       alert(
-      //         "Sorry, contact is in development. For now please email me directly at luke.ab.johnson@gmail.com"
-      //       );
-      //     }}
-      //   >
-      //     Submit
-      //   </button>
-      // </div> 
