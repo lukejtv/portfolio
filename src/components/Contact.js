91ebe42c8960530c98adafe076922d9d10c7165c
ChaseRensberger <chasehrensberger@gmail.com>
2022-06-02 00:26:03 -0400

    please no more warnings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,22 +12,22 @@ export default Contact;
 
 
 
-      {/* <div className="contact-container">
-        <div className="contact-title-text">Contact</div>
-        <div className="contact-sub-text">
-          For freelance projects, portfolio questions, and more.
-        </div>
-        <input className="contact-name-and-email" placeholder="Name" />
-        <input className="contact-name-and-email" placeholder="Email" />
-        <textarea className="contact-message" placeholder="Message" />
-        <button
-          className="contact-submit"
-          onClick={() => {
-            alert(
-              "Sorry, contact is in development. For now please email me directly at luke.ab.johnson@gmail.com"
-            );
-          }}
-        >
-          Submit
-        </button>
-      </div> */}
+      //  <div className="contact-container">
+      //   <div className="contact-title-text">Contact</div>
+      //   <div className="contact-sub-text">
+      //     For freelance projects, portfolio questions, and more.
+      //   </div>
+      //   <input className="contact-name-and-email" placeholder="Name" />
+      //   <input className="contact-name-and-email" placeholder="Email" />
+      //   <textarea className="contact-message" placeholder="Message" />
+      //   <button
+      //     className="contact-submit"
+      //     onClick={() => {
+      //       alert(
+      //         "Sorry, contact is in development. For now please email me directly at luke.ab.johnson@gmail.com"
+      //       );
+      //     }}
+      //   >
+      //     Submit
+      //   </button>
+      // </div> 
