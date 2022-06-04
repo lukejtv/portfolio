c72c6f38726b7c54059f547b14f0068092089fdc
ChaseRensberger <chasehrensberger@gmail.com>
2022-06-04 03:06:16 -0400

    sorry

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <div className="contact-title-text">Contact</div>
-        <div className="contact-sub-text">Coming soon.</div>
+        <div className="contact-title-text">Contact.</div>
+        <div className="contact-sub-text">luke.ab.johnson@gmail.com</div>
       </div>
     </div>
   );
