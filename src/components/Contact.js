f3d6061cd4ff13b5438a62d7c2da0c26f79ae80b
ChaseRensberger <chasehrensberger@gmail.com>
2022-06-04 20:07:36 -0400

    add contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,20 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <div className="contact-title-text">Contact.</div>
-        <div className="contact-sub-text">luke.ab.johnson@gmail.com</div>
+        <div>
+          <div className="contact-title-text">Contact.</div>
+          <div className="contact-sub-text">
+            For portfolio questions, freelance projects, and more.
+          </div>
+        </div>
+        <input className="contact-name-email-subject" placeholder="Name" />
+        <input className="contact-name-email-subject" placeholder="Email" />
+        <input className="contact-name-email-subject" placeholder="Subject" />
+        <textarea className="contact-message" placeholder="Message" />
+        <button className="contact-submit">Submit.</button>
+        <div className="credit">
+          Website Designed by LUKEJ | Engineered by CHASE RENSBERGER
+        </div>
       </div>
     </div>
   );
